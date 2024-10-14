ff93a1084634fe6fc8a8afb021e9795a1e5204da
Михаил Белоусов <mike_ussoff@mail.ru>
2024-10-14 14:36:10 +0300

    additional variant

--- hw03_searchforstockitems v2.sql
+++ hw03_searchforstockitems v2.sql
@@ -1,28 +1,28 @@
 create or replace function warehouse.searchforstockitems (
 	in searchtext varchar(1000),
 	in maximumrowstoreturn int)
-returns varchar
+returns table (items json)
 language plpgsql 
 security definer
 as $$
 declare
 	res text;
 begin
+return query
 select row_to_json(t)
 from 
 (
 	select array_to_json(array_agg(row_to_json(d))) as stockitems
 	from 
 	(
-		select stockitemid, stockitemname
+    	select stockitemid, stockitemname
 		from warehouse.stockitems	
 		where searchdetails ilike '%' || searchtext || '%'
 		order by stockitemname
 		limit maximumrowstoreturn
 	) d
-)t into res;
-return res;
+)t;
 end;
 $$;
 
-select warehouse.searchforstockitems('JOKE', 100) as json;
+select * from warehouse.searchforstockitems('JOKE', 100);
